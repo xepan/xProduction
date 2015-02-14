82716ba857582e987998adab25353f8ef9a8332d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-02-14 16:54:07 +0530

    Working on JobCards

---
+++ page/owner/dept/completed.php
@@ -0,0 +1,11 @@
+<?php
+
+class page_xProduction_page_owner_dept_completed extends page_xProduction_page_owner_dept_base{
+	
+	function init(){
+		parent::init();
+		
+
+
+	}
+}
---
+++ page/owner/dept/processed.php
@@ -0,0 +1,11 @@
+<?php
+
+class page_xProduction_page_owner_dept_processed extends page_xProduction_page_owner_dept_base{
+	
+	function init(){
+		parent::init();
+		
+
+
+	}
+}

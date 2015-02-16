c73ad2b9a292c14b32473517426d78e9185e6ff6
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-02-16 16:30:08 +0530

    misc

--- lib/Model/JobCard.php
+++ lib/Model/JobCard.php
@@ -14,7 +14,38 @@ class Model_JobCard extends \SQL_Model{
 
 		$this->addField('name')->caption('Job Number');
 		$this->addField('status')->enum(array('-','received','approved','assigned','processing','processed','completed','forwarded'))->defaultValue('-');
+
+		$this->hasMany('xProduction/JobCardEmployeeAssociation','jobcard_id');
+
 		$this->add('dynamic_model/Controller_AutoCreator');
 
+	}
+
+	function assignTo($employee){
+		// create log/communication 
+		$temp=$this->add('xProduction/Model_JobCardEmployeeAssociation');
+		$temp->addCondition('jobcard_id',$this->id);
+		$temp->addCondition('employee_id',$employee->id);
+		$temp->tryLoadAny();
+		$temp->save();
+
+		$this['status']='assigned';
+		$this->saveAs('xProduction/Model_JobCard');
+		// throw $this->exception('To Do');
+	}
+
+	function getAssociatedEmployees(){
+		$associate_employees= $this->ref('xProduction/JobCardEmployeeAssociation')->_dsql()->del('fields')->field('employee_id')->getAll();
+		return iterator_to_array(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($associate_employees)),false);
+	}
+
+	function removeAllEmployees(){
+		$this->ref('xProduction/JobCardEmployeeAssociation')->deleteAll();
+		$this['status']='received';
+		$this->save();
+	}
+
+	function start_processing(){
+		
 	}
 }	
--- page/owner/dept/base.php
+++ page/owner/dept/base.php
@@ -4,12 +4,5 @@ class page_xProduction_page_owner_dept_base extends page_xProduction_page_owner_
 	public $department;
 	function init(){
 		parent::init();
-
-		if(!$this->api->stickyGET('department_id'))
-			throw $this->exception("Department ID must be specified");
-
-		$this->department = $this->add('xHR/Model_Department')->load($_GET['department_id']);
-			
-
 	}
 }

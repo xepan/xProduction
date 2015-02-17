a7abbc2c7983e6f3d86e12c8dbfa26132b29b11b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-02-17 16:40:11 +0530

    acl controller added

--- page/owner/dept/assigned.php
+++ page/owner/dept/assigned.php
@@ -5,6 +5,12 @@ class page_xProduction_page_owner_dept_assigned extends page_xProduction_page_ow
 	function init(){
 		parent::init();
 		
+		
+		$crud=$this->add('CRUD',array('grid_class'=>'xShop/Grid_Quotation'));
+		$crud->setModel('xShop/Quotation_Assigned');
+		
+
+		$crud->add('xHR/Controller_Acl');
 
 
 	}
--- page/owner/dept/processed.php
+++ page/owner/dept/processed.php
@@ -5,7 +5,11 @@ class page_xProduction_page_owner_dept_processed extends page_xProduction_page_o
 	function init(){
 		parent::init();
 		
+		$crud=$this->add('CRUD',array('grid_class'=>'xShop/Grid_Quotation'));
+		$crud->setModel('xShop/Quotation_Processed');
+		
 
+		$crud->add('xHR/Controller_Acl');
 
 	}
 }
